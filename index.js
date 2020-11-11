862b96fed175d9c7bb046950f8a54e4d29e69a30
Leo <leo.chen@cloud-interactive.com>
2020-11-11 11:18:28 +0800

    add post fields

--- index.js
+++ index.js
@@ -7,12 +7,6 @@ const { PossibleFragmentSpreadsRule } = require('graphql');
 const typeDefs = gql`
   # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
 
-  # This "Book" type defines the queryable fields for every book in our data source.
-  type Book {
-    title: String
-    author: String
-  }
-
   # 高度
   enum HeightUnit {
     METRE
@@ -27,14 +21,24 @@ const typeDefs = gql`
     POUND
   }
 
-
-  # this "User" type
+  # This "User" type defines the queryable fields for every user in our data source.
   type User {
+    id: Int
     name: String!
     age: Int
     friends: [User]
+    posts: [Post]
     height(unit: HeightUnit = CENTIMETER): Float
     weight(unit: WeightUnit = KILOGRAM): Float
+  }
+
+  # This "Post" type
+  type Post {
+    id: Int!
+    author: User
+    title: String
+    content: String
+    likeGivers: [User]
   }
 
   # The "Query" type is special: it lists all of the available queries that
@@ -42,33 +46,23 @@ const typeDefs = gql`
   # case, the "books" query returns an array of zero or more Books (defined above).
   type Query {
     self: User
-    books: [Book]
+    user(id: Int!): User
     users: [User]
-    user(name: String!): User
     usersHeight(unit: HeightUnit = CENTIMETER): [Float]
   }
 `;
 
-const books = [
-  {
-    title: 'The Awakening',
-    author: 'Kate Chopin',
-  },
-  {
-    title: 'City of Glass',
-    author: 'Paul Auster',
-  },
-];
-
 const users = [
   {
+    id: 1,
     name: 'leo',
     age: 20,
-    friends: ['woody'],
+    friends: [2],
     height: 175,
     weight: 75
   },
   {
+    id: 2,
     name: 'woody',
     age: 20,
     friends: [],
@@ -76,6 +70,12 @@ const users = [
     weight: 60
   }
 ]
+
+const posts = [
+  { id: 1, authorId: 1, title: "1", content: "This is my first post.", likeGiverIds: [2] },
+  { id: 2, authorId: 2, title: "2", content: "My name is woody!!", likeGiverIds: [1] },
+  { id: 3, authorId: 1, title: "3", content: "Here's my second post.", likeGiverIds: [] },
+];
 
 const HeightValue = {
   METRE: 100,
@@ -93,22 +93,34 @@ const WeightValue = {
 // schema. This resolver retrieves books from the "books" array above.
 const resolvers = {
   Query: {
-    self: () => users.find(user => user.name === 'leo'),
-    books: () => books,
+    self: () => users.find(user => user.id === 1),
+    user: (root, args, context) => users.find(user => user.id === args.id),
     users: () => users,
-    usersHeight: (root, args, context) => users.map(item => getValue('height', item.height, args.unit)),
-    user: (root, args, context) => users.find(user => user.name === args.name)
+    usersHeight: (root, args, context) => users.map(item => getValue('height', item.height, args.unit))
   },
   User: {
-    friends: (parent) => users.filter(user => parent.friends.includes(user.name)),
+    friends: (parent) => users.filter(user => parent.friends.includes(user.id)),
+    posts: (parent) => posts.filter(post => post.authorId === parent.id),
     height: (parent, args) => getValue('height', parent.height, args.unit),
     weight: (parent, args) => getValue('weight', parent.weight, args.unit)
+  },
+  Post: {
+    author: (parent) => users.find(user => user.id === parent.id),
+    likeGivers: (parent) => parent.likeGiverIds.map(id => users.find(user => user.id === id)),
   }
 };
 
 // The ApolloServer constructor requires two parameters: your schema
 // definition and your set of resolvers.
 const server = new ApolloServer({ typeDefs, resolvers });
+
+// The `listen` method launches a web server.
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
+
+
+// utils
 
 const getValue = (type, value, unit) => {
   const unitValues = type === 'weight' ? WeightValue : HeightValue
@@ -118,8 +130,3 @@ const getValue = (type, value, unit) => {
   }
   return value * unitValues[unit]
 }
-
-// The `listen` method launches a web server.
-server.listen().then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);
-});
